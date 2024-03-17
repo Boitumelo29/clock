f5eb530efbd96a719e6504337d27bf61466d1945
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-03-17 18:34:30 +0200

    update

---
+++ main/script.js
@@ -0,0 +1,9 @@
+const numberHours = document.querySelector('.number-hours');
+
+const numberElement = [];
+
+for(let i = 1; i <= 12; i++){
+    numberElement.push(`<span style="--index: ${i};"><p>${i}</p></span>`);
+}
+
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
